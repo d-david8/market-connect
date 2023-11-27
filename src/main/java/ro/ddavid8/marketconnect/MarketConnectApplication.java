fba0c5da45a1c641100f08aa42b6934cb9c85712
Dan David <d_david8@yahoo.com>
2023-11-27 19:18:58 +0200

    first commit

---
+++ src/main/java/ro/ddavid8/marketconnect/MarketConnectApplication.java
@@ -0,0 +1,13 @@
+package ro.ddavid8.marketconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/ddavid8/marketconnect/MarketConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.ddavid8.marketconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
